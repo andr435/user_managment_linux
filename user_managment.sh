d23703b4ce724bbb4ee6ccab58e34c40bb06b006
andr435 <andr435@gmail.com>
2025-03-01 01:58:40 +0200

    init project

---
+++ user_managment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+################################################
+# Developed by: Andrey M.
+# Purpose: User managment on linux machine
+# Date: 01-03-2025
+# Version: 1.0.0
+################################################
+set -o errexit
+set -o pipefail
+set -o nounset
+################################################
+
+
