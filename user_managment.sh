0c1b554eec1ca2c0570c003275aba193550186d6
andr435 <andr435@gmail.com>
2025-03-01 04:34:59 +0200

    chapter 27 & 28

--- user_managment.sh
+++ user_managment.sh
@@ -11,4 +11,69 @@ set -o pipefail
 set -o nounset
 ################################################
 
+################################################
+# Chapter 27
+################################################
+
+# 1.
+who am i
+
+# 2.
+who
+
+# 3.
+w
+
+# 4.
+id | awk '{print $1}'
+
+# 5.
+su vasia
+exit
+
+# 6.
+su - vasia
+exit
+
+# 7.
+useradd -m vasia
+
+# 8.
+sudo useradd -m vasia
+
+
+#####################################################
+# Chapter 28
+#####################################################
+
+# 1.
+sudo useradd -m -c "Serena Williams" serena 
+
+# 2.
+sudo useradd -m -c "Venus Williams" -s /bin/bash venus
+
+# 3.
+sudo cat  /etc/passwd /etc/shadow /etc/group | grep -E 'serena|venus'
+
+# 4.
+ls /home
+
+# 5.
+sudo useradd -m -s /bin/date einstime
+
+# 7.
+
+# 8.
+sudo cat "Welcone!!!!" > /etc/skel/welcome.txt
+
+# 9.
+sudo useradd -m testuser
+sudo userdel -r testuser
+
+# 10.
+grep 'serena' /etc/passwd
+sudo chsh -s /usr/bash
+grep 'serena' /etc/passwd
+
+
 
